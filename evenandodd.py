8db581de33932148c44db6ff03a15c41d99932b4
Fazil Abdullah Mohammad <fazilabdullah943@gmail.com>
2024-07-12 07:52:08 +0530

    first commit

---
+++ evenandodd.py
@@ -0,0 +1,15 @@
+# class number:
+#     def __init__(self,n):
+#         if n<0:
+#             print("cannot be determined")
+#         elif n%2==0:
+#                 print("even")
+#         else:
+#                 print("odd")
+        
+
+# n=int(input())
+# ob=number(n)
+
+        
+
---
+++ oops.py
@@ -0,0 +1,9 @@
+class a:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+        print(a+b)
+    def print1(self):
+        print(self.a+self.b)
+ob=a(10,20)
+ob.print1()
